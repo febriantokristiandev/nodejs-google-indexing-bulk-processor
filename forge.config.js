a5ae7ff549d0cad2e0022429d4497e6d650bd689
febrianto-2000 <febrians.real@gmail.com>
2024-07-13 16:20:29 +0700

    update

--- forge.config.js
+++ forge.config.js
@@ -1,16 +1,16 @@
 module.exports = {
     packagerConfig: {
-      icon: './path/to/icon', // Path to your app icon
+        icon: './img/icon.ico',
     },
     makerConfig: {
-      squirrelWindows: {
-        name: "@electron-forge/maker-squirrel",
-        icon: './path/to/icon.ico', // Path to your Windows icon
-      },
-      zip: {
-        name: "@electron-forge/maker-zip",
-        // Add additional zip configuration if needed
-      },
+        squirrelWindows: {
+            name: "@electron-forge/maker-squirrel",
+            icon: './img/icon.ico', 
+        },
+        zip: {
+            name: "@electron-forge/maker-zip",
+            icon: './img/icon.ico',
+        },
     },
-  };
+};
   
